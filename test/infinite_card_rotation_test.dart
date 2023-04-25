eaed06190c7841f1f65f901c5dc4b913b583e0e9
Rohit Joshi <rohit.joshi@quantive.com>
2023-04-25 17:16:17 +0530

    Added Examples

--- test/infinite_card_rotation_test.dart
+++ test/infinite_card_rotation_test.dart
@@ -1,13 +1,8 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:infinite_card_rotation/infinite_card_rotation.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = FlipCard(
-      front: Container(),
-      back: Container(),
-    );
+
   });
 }
